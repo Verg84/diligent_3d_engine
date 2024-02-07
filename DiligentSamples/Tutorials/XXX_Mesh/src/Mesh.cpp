a40d0137cd2efbd36857523d563133523bb78538
Verg84 <50481854+Verg84@users.noreply.github.com>
2024-02-07 12:32:51 +0100

    mesh proj

---
+++ DiligentSamples/Tutorials/XXX_Mesh/src/Mesh.cpp
@@ -0,0 +1,22 @@
+#include"Mesh.hpp"
+
+namespace Diligent
+{
+	SampleBase* CreateSample()
+	{
+        return new Mesh();
+    }
+    void Mesh::Initialize(const SampleInitInfo& InitInfo)
+    {
+        SampleBase::Initialize(InitInfo);
+    }
+    void Mesh::Render()
+    {
+
+    }
+    void Mesh::Update(double ctime, double etime)
+    {
+        SampleBase::Update(ctime, etime);
+    }
+
+    }
