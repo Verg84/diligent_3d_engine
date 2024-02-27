f86b443a7cc1b05a695fce744ca37ad45372f5c2
Verg84 <50481854+Verg84@users.noreply.github.com>
2024-02-27 11:07:20 +0100

    project instancing

---
+++ DiligentSamples/Tutorials/XXX_PROJECTS/XXX_INSTANCING/src/Instancing.cpp
@@ -0,0 +1,21 @@
+#include "Instancing.hpp"
+
+namespace Diligent
+{
+	SampleBase* CreateSample()
+	{
+        return new Instancing();
+	}
+	void Instancing::Initialize(const SampleInitInfo &InitInfo)
+	{
+        SampleBase::Initialize(InitInfo);
+	}
+	void Instancing::Render()
+	{
+
+	}
+	void Instancing::Update(double ctime,double etime)
+	{
+        SampleBase::Update(ctime, etime);
+	}
+}
---
+++ DiligentSamples/Tutorials/XXX_PROJECTS/XXX_INSTANCING/src/Instancing.hpp
@@ -0,0 +1,16 @@
+#include "SampleBase.hpp"
+
+namespace Diligent
+{
+	class Instancing final :public SampleBase
+	{
+    public:
+        virtual void Initialize(const SampleInitInfo& InitInfo) override final;
+        virtual void Render() override final;
+        virtual void Update(double ctime, double etime) override final;
+        virtual const Char* GetSampleName()const override final
+        {
+            return "XXX::Instancing";
+        }
+	};
+}
